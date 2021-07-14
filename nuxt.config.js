a24fccd4d84768b0edfc43bac8109fc4c78452f5
Mark Scherrenberg <m.scherrenberg@programic.com>
2021-07-14 19:32:21 +0200

    Config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'server',
 
+  server: {
+    host: '0.0.0.0'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'rocksolid-headless-shopify',
