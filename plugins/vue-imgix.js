c1ee4aef0f8b022389aa14d5f92585a29ca8d40c
Mark Scherrenberg <m.scherrenberg@programic.com>
2021-07-25 21:24:17 +0200

    Change imgix domain

--- plugins/vue-imgix.js
+++ plugins/vue-imgix.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueImgix from 'vue-imgix';
 
 Vue.use(VueImgix, {
-  domain: "lidekahome.imgix.net",
+  domain: "lideka-home.imgix.net",
   defaultIxParams: {
     auto: 'format,compress'
   },
