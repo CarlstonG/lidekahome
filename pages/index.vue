0d52e56282f2ac272eca8453b582b2e6a1665ba7
Daan Kloetstra <daankloetstra@hotmail.com>
2022-01-07 15:04:10 +0100

    update home

--- pages/index.vue
+++ pages/index.vue
@@ -27,7 +27,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M13 16V6a1 1 0 00-1-1H4a1 1 0 00-1 1v10a1 1 0 001 1h1m8-1a1 1 0 01-1 1H9m4-1V8a1 1 0 011-1h2.586a1 1 0 01.707.293l3.414 3.414a1 1 0 01.293.707V16a1 1 0 01-1 1h-1m-6-1a1 1 0 001 1h1M5 17a2 2 0 104 0m-4 0a2 2 0 114 0m6 0a2 2 0 104 0m-4 0a2 2 0 114 0"/>
             </svg>
-            <p>Voor 23:59 besteld, morgen in huis</p>
+            <DeliveryTime />
           </div>
 
           <div class="flex items-center text-sm font-medium text-indigo-400">
@@ -376,6 +376,7 @@ import VideoReviews from "~/components/VideoReviews.vue";
 import Lenghts from "~/components/Lenghts.vue";
 import Calendly from '~/components/Calendly.vue';
 import Newsletter from '~/components/NewsletterBlock.vue';
+import DeliveryTime from '~/components/DeliveryTime.vue';
 
 export default Vue.extend({
   components: {
@@ -388,6 +389,7 @@ export default Vue.extend({
     ProductItemGradient,
     Calendly,
     Newsletter,
+    DeliveryTime,
   },
   
   data() {
