d3e1f593503964277754cf23844b8be5ee0ee07c
Daan Kloetstra <daankloetstra@hotmail.com>
2022-02-09 11:35:27 +0100

    Replace

---
+++ pages/services/verify_email/confirm.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="bg-white relative flex justify-center py-10">
+        <Loading />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Loading from "~/components/Loading.vue";
+
+export default Vue.extend({
+    components: {
+        Loading,
+    },
+    
+    mounted() {
+        window.location.replace(`https://checkout.lidekahome.nl/services/verify_email/confirm${window.location.search}`);
+    }
+})
+</script>
