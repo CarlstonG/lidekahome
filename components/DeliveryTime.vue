a301d3243f2b9f3001b53975a49fb7078a514ad7
Daan Kloetstra <daankloetstra@hotmail.com>
2022-04-28 07:58:41 +0200

    Update

--- components/DeliveryTime.vue
+++ components/DeliveryTime.vue
@@ -15,7 +15,6 @@ export default Vue.extend({
         },
 
         deliveryTime() {
-            return 'Vandaag besteld vrijdag thuis';
             switch (this.weekDayNumber) {
                 case 1:
                 case 2:
